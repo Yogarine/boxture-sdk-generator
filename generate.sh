926cd05e11f048aa14bcd4389a1d220396856b7b
Alwin Garside <alwin@garsi.de>
2019-06-04 15:02:47 +0200

    Fixes and sumodule

--- generate.sh
+++ generate.sh
@@ -10,9 +10,11 @@ function codegen {
         --input-spec     "$3" \
         --output         "/local/$1" \
         --invoker-package "$4" \
-        --additional-properties "composerProjectName=$1,gitRepoId=$1,artifactVersion=$2,packageVersion=$2"
+        --additional-properties "composerProjectName=$1,gitRepoId=$1,artifactVersion=$2,packageVersion=$2" \
+        -DapiTests=false \
+        -DmodelTests=false
 
     echo ""
 }
 
-codegen "boxture-$GENERATOR-sdk" '2.0.0' 'https://staging.localexpress.nl/api/v2/swagger_doc.json' 'Boxture'
+codegen "boxture-v2-$GENERATOR-sdk" '0.1.0' 'https://staging.localexpress.nl/api/v2/swagger_doc.json' 'Boxture\V2'
